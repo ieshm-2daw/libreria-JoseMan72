20d5583149f62bc953a3f83c4eb2d2cf91fa2652
JoseMan72 <josemanuelbonillagarcia@gmail.com>
2023-11-23 14:26:36 +0100

    Modelos completados

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -14,10 +14,51 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     dni = models.CharField(max_length=9, unique=True)
-    direccion = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=9)
+    direccion = models.TextField()
+    telefono = models.IntegerField()
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autores = models.ManyToManyField('Autor')
+    editorial = models.ForeignKey('Editorial', on_delete=models.CASCADE)
+    fecha_publicacion = models.DateField()
+    
+    #genero tendrá opciones de selección, por ejemplo: novela, ensayo, cuento, etc.
+    GENEROS = (
+        ('N', 'Novela'),
+        ('C', 'Cuento'),
+        ('P', 'Poesía'),
+        ('T', 'Teatro'),
+        ('O', 'Otros'),
+    )
+    genero = models.CharField(max_length=1, choices=GENEROS)
+    isbn = models.IntegerField(max_length=13, unique=True)
+    resumen = models.TextField()
+
+    DISPONIBILIDAD = (
+        ('D', 'Disponible'),
+        ('P', 'Prestado'),
+    )
+    disponibilidad = models.CharField(max_length=1, choices=DISPONIBILIDAD)
+    #portada = models.ImageField() #Neceista Pillow
 
 class Autor(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, unique=True)
     biografia = models.TextField()
-    #foto = models.ImageField() #Hace falta instalar Pillow
+    #foto = models.ImageField() #Necesita Pillow
+
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    direccion = models.TextField()
+    sitio_web = models.URLField() #default='https://es.wikipedia.org/wiki/'
+
+class Prestamo(models.Model):
+    libro = models.ForeignKey('Libro', on_delete=models.CASCADE)
+    fecha_prestamo = models.DateField()
+    fecha_devolucion = models.DateField()
+    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    ESTADO = (
+        ('P', 'Prestado'),
+        ('D', 'Devuelto'),
+    )
+    estado = models.CharField(max_length=1, choices=ESTADO)
