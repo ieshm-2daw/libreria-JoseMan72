7627443a8ec49c2bf959c57491c4a84e5ed1ae6a
jose manuel <josemanuelbonillagarcia@gmail.com>
2023-11-30 11:15:26 +0100

    Libro disponibles y presados divididos

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -6,10 +7,29 @@ from .models import Libro
 # Create your views here.
 
 # lista, detalle y modificar
-
+'''
 class LibroListView(ListView):
     model = Libro
     template_name = 'biblioteca/libro_list.html'
+'''
+# librolistview pero que solo muestre los libros disponibles
+class LibroListView(ListView):
+    model = Libro
+    template_name = 'biblioteca/libro_list.html'
+
+    #queryset = Libro.objects.filter(disponibilidad='D')
+    ''' Otra forma sobrescribiendo el metodo get_queryset
+    def get_queryset(self): #sobreescribimos el metodo get_queryset, que sirve para filtrar los libros disponibles pasandole el parametro 'D'
+        return Libro.objects.filter(disponibilidad='D')
+    '''
+
+    #filtrar los libros disponibles y no disponibles
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['libros_disponibles'] = Libro.objects.filter(disponibilidad='D')
+        context['libros_prestados'] = Libro.objects.filter(disponibilidad='P')
+
+        return context
 
 class LibroDetailView(DetailView):
     model = Libro
